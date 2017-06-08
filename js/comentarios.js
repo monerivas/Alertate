356bf4c980cd2507d527f7212be7f7bc81aab3eb
Daffyta <daffy.dine9@gmail.com>
2017-06-08 15:35:59 -0500

    Agregando comentarios

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,36 +1,23 @@
 var formulario = function(){
-	$("#comentarios").submit(agregarTweet);
-  $("#textarea1").keyup(validarTexto);
-
+	$("#enviar").click(agregarComentario);
 };
 
-var agregarTweet = function(e){
+var agregarComentario = function(e){
     e.preventDefault();
-
     //Obtención de elementos
-    var $postContenedor = $("<section />", { "id": "aqui-comentarios" });
+    var $postContenedor = $("<article />", { "class": "mi-comentario container" });
     var $postTexto = $("<p />");
-
+    var $denuncia = $("#textarea1").val();
     //Personalización de elementos
-    $postTexto.text($("#textarea1").val());
+    $postTexto.text($denuncia);
 
     // Agregar al DOM
     $postContenedor.append($postTexto);
     // Agregar a un elemento existente para visualizarlo
     $("#aqui-comentarios").append($postContenedor);
+
     // Borrar contenido de textarea
     $("#textarea1").val("");
-    validarTexto();
-};
-
-var validarTexto = function(){
-    var $caracteres = conteoCaracteres();
-    colores(Number($caracteres));
-    	if($caracteres < 140 && $caracteres > -1){
-    		$("#enviar").removeAttr("disabled");
-    	} else {
-    		$("#enviar").attr("disabled", true);
-    	}
 };
 
 $(document).ready(formulario);
